0c052ac1f6d8fadd32cef89e3b620a06df67606b
bkhanal87@gmail.com <bkhanal87@gmail.com>
2023-06-19 20:53:26 -0400

    create two variables and add them

---
+++ src/VariablesInJava.java
@@ -0,0 +1,14 @@
+public class VariablesInJava {
+    public int a;
+    public int b;
+
+    public void add(int a, int b) {
+        System.out.println(a +  b);
+    }
+    public static void main(String[] args) {
+        VariablesInJava v = new VariablesInJava();
+        v.add(3, 4);
+    }
+}
+
+
